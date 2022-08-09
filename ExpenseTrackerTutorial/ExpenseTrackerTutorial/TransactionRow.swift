72c114cd95cf186edd840aa5190946c35d830566
one1color <koki.111.sui@gmail.com>
2022-08-09 13:04:32 +0200

    part4 done, transactionListView, 57:16

--- ExpenseTrackerTutorial/ExpenseTrackerTutorial/TransactionRow.swift
+++ ExpenseTrackerTutorial/ExpenseTrackerTutorial/TransactionRow.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import SwiftUIFontIcon
 
 struct TransactionRow: View {
     var transaction: Transaction
     
     var body: some View {
         HStack(spacing: 20) {
+            // MARK: Category Icon
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(Color.icon.opacity(0.3))
+                .frame(width: 44, height: 44)
+                .overlay {
+                    FontIcon.text(.awesome5Solid(code: transaction.icon), fontsize: 24, color: Color.icon)
+                }
+            
             VStack(alignment: .leading, spacing: 6) {
                 // MARK: Transaction merchant
                 Text(transaction.merchant)
