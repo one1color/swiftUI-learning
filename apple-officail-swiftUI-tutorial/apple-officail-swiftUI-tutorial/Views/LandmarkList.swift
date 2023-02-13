950dde4844e0fb36f2ecad726119b9d48b237b2d
one1color <koki.111.sui@gmail.com>
2023-02-13 16:54:16 +0100

    Building Lists and Navigation

---
+++ apple-officail-swiftUI-tutorial/apple-officail-swiftUI-tutorial/Views/LandmarkList.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkList.swift
+//  apple-officail-swiftUI-tutorial
+//
+//  Created by koki isshiki on 13.02.23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone 14 Pro", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
