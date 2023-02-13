acde964003794e7c625c11635c10144c21587f9d
one1color <koki.111.sui@gmail.com>
2023-02-13 15:37:00 +0100

    Creating Combining Views

---
+++ apple-officail-swiftUI-tutorial/apple-officail-swiftUI-tutorial/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  apple-officail-swiftUI-tutorial
+//
+//  Created by koki isshiki on 13.02.23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ apple-officail-swiftUI-tutorial/apple-officail-swiftUI-tutorial/apple_officail_swiftUI_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_officail_swiftUI_tutorialApp.swift
+//  apple-officail-swiftUI-tutorial
+//
+//  Created by koki isshiki on 13.02.23.
+//
+
+import SwiftUI
+
+@main
+struct apple_officail_swiftUI_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
